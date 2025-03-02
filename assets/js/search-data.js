c06f9f28b1c95852a7480427683ea2d7afbe949f
xvyaward <ckdgnsrhkspd@naver.com>
2025-03-02 08:38:46 +0000

    Deploying to gh-pages from @ xvyaward/xvyaward.github.io@e74b74749e6fa1b65f92e84b896a0fb68431e2d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
