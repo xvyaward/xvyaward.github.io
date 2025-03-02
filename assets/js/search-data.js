990676fa9093e8554f5387c2e1162ec64b7f77e5
xvyaward <ckdgnsrhkspd@naver.com>
2025-03-02 08:26:00 +0000

    Deploying to gh-pages from @ xvyaward/xvyaward.github.io@61bc6bfb20999ffcad2b872661ae447632af522c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,14 +459,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%63%68%61%6E%67%68%75%6E.%6C%65%65@%70%6F%73%74%65%63%68.%61%63.%6B%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/xvyaward", "_blank");
+        },
+      },{
+        id: 'social-instagram',
+        title: 'Instagram',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://instagram.com/changhun__le", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/chang-hun-lee", "_blank");
         },
       },{
         id: 'social-rss',
@@ -480,14 +494,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=xkQD3YYAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/changhunlee_", "_blank");
         },
       },{
       id: 'light-theme',
